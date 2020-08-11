77c8d2184e7d2c100cf7bdb87439a0c130c5b8c3
softbroz <softbroz@gmail.com>
2020-08-11 14:34:52 +0500

    Section 9: 132. Understanding Dynamic Component Behavior

--- src/components/New.vue
+++ src/components/New.vue
@@ -3,13 +3,22 @@
         <h3>
             New Quote
         </h3>
+        <button @click="counter++">Increase</button>
+        <p>{{counter}}</p>
     </div>
 </template>
 
 
 <script>
 export default {
-  
+  data: function(){
+      return {
+          counter: 0
+      };
+  },
+  destroyed(){
+      console.log("Destroyed!");
+  }
 }
 </script>
 
